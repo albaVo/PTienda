f3af4c927b34aca7aa78e817e68424d1a6af23ef
DESKTOP-AMN55HB\alba1 <alba12vo@gmail.com>
2023-02-21 22:54:44 +0100

    Hola mundo

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 
-# echo "Tareas (por etapas) del pipeline JenkinsFile..."
+echo "Tareas (por etapas) del pipeline JenkinsFile..."
 
-# echo "Instalando la tienda App PC Componentes"
-# echo "Descargar respositorio de AppTienda"
-# echo "Actualizar versiones de los dockerfiles en Docker Hub..."
-# echo "Realizaar el build del proyecto..."
-# echo "Fases del testeo..."
-# echo "Matar los contenedores en ejecución de la versión anterior..."
-# echo "Levantar los nuevos contenedores actualizados..."
+echo "Instalando la tienda App PC Componentes"
+echo "Descargar respositorio de AppTienda"
+echo "Actualizar versiones de los dockerfiles en Docker Hub..."
+echo "Realizaar el build del proyecto..."
+echo "Fases del testeo..."
+echo "Matar los contenedores en ejecución de la versión anterior..."
+echo "Levantar los nuevos contenedores actualizados..."
 
-echo "Desplegando la app de pTienda"
+# echo "Desplegando la app de pTienda"
 
-docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d
+# docker-compose -f docker-compose.yml build
+# docker-compose -f docker-compose.yml up -d
