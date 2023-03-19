552502875d9b8d7cd43317f8064e2e712dea29bc
DESKTOP-AMN55HB\alba1 <alba12vo@gmail.com>
2023-03-20 00:49:18 +0100

    prueba

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 
 echo "Desplegando la app de pTienda"
 
-# docker-compose -f docker-compose.yml down
-docker stop apiTiendaNest
-docker rm apiTiendaNest
+docker-compose -f docker-compose.yml down
 
 
 docker-compose -f docker-compose.yml build
