15bbadb854b499f3eb6672020bbf3eafd6245e9f
DESKTOP-AMN55HB\alba1 <alba12vo@gmail.com>
2023-03-20 00:07:14 +0100

    prueba

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ sudo rm -rf postgres
 sudo rm -rf web
 
 
-echo "Desplegando la app de pTienda"
+# echo "Desplegando la app de pTienda"
 
-docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d
-# docker exec -it apiNest /bin/bash
+# docker-compose -f docker-compose.yml build
+# docker-compose -f docker-compose.yml up -d
+# # docker exec -it apiNest /bin/bash
