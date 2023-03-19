205855a46419c60f02e4eaa8bbc7addd8b5877f2
DESKTOP-AMN55HB\alba1 <alba12vo@gmail.com>
2023-03-20 00:06:37 +0100

    prueba

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
+docker-compose -f docker-compose.yml down
+docker rmi apitienda:latest 
+docker image prune
 
-# echo "Tareas (por etapas) del pipeline JenkinsFile..."
+sudo rm -rf postgres
+sudo rm -rf web
 
-# echo "Instalando la tienda App PC Componentes"
-# echo "Descargar respositorio de AppTienda"
-# echo "Actualizar versiones de los dockerfiles en Docker Hub..."
-# echo "Realizaar el build del proyecto..."
-# echo "Fases del testeo..."
-# echo "Matar los contenedores en ejecución de la versión anterior..."
-# echo "Levantar los nuevos contenedores actualizados..."
 
 echo "Desplegando la app de pTienda"
 
