5741971df65a9742b322e4a84e2f5e2bfe5f24ac
DESKTOP-AMN55HB\alba1 <alba12vo@gmail.com>
2023-03-20 00:56:30 +0100

    prueba

--- install.sh
+++ install.sh
@@ -4,14 +4,6 @@ echo "Desplegando la app de pTienda"
 # docker-compose -f docker-compose.yml down
 
 
-# docker-compose -f docker-compose.yml build
-# docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml up -d
 # docker exec -it apiNest /bin/bash
-
-cd /var/lib/jenkins/workspace/Proyectos/Job-pTienda/api_nest/
-
-export $(cat .env | xargs)
-
-docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/api_nest/docker-compose.yml build --no-cache
-
-docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/api_nest/docker-compose.yml up -d
